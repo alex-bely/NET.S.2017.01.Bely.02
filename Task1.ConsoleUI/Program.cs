a59ceec6a0194642937b312d90d3e011a5e34ef4
alex-bely <alex.bely.420601@gmail.com>
2017-03-12 18:15:16 +0300

    Task1-solution

---
+++ Task1.ConsoleUI/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using static Task1.MergeSort;
+namespace Task1.ConsoleUI
+{
+    /// <summary>
+    ///  Uses console user interface to display
+    ///  sorted arrays
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Entry point of the application
+        /// </summary>
+        static void Main(string[] args)
+        {
+            //Initialization of arrays
+            int[] array = new int[] { };
+            int[] array_2 = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] array_3 = new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            
+            //Sorting arrays
+            Sort(array);
+            Sort(array_2);
+            Sort(array_3);
+
+            //Display the elements of the arrays
+            Console.WriteLine("1st array");
+            foreach (var number in array)
+                Console.Write(number+" ");
+            Console.WriteLine();
+
+            Console.WriteLine("2nd array");
+            foreach (var number in array_2)
+                Console.Write(number + " ");
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine("3rd array");
+            foreach (var number in array_3)
+                Console.Write(number + " ");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task1/MergeSort.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+
+namespace Task1
+{
+
+    /// <summary>
+    /// Provides method that 
+    /// implements merge sorting algorithm. 
+    /// </summary>
+    public static class MergeSort
+    {
+        /// <summary>
+        /// Sorts the elements in a one-dimensional array of integer
+        /// </summary>
+        /// <param name="array">The one-dimensional integer array to sort.</param>
+        public static void Sort(int[] array)
+        {
+            Sort(array, 0, array.Length - 1);
+        }
+
+        private static void Sort(int[] input, int left, int right)
+        {
+
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+
+                Sort(input, left, middle);
+                Sort(input, middle + 1, right);
+
+                Merge(input, left, right, middle);
+
+            }
+        }
+
+
+        private static void Merge(int[] input, int left, int right, int middle)
+        {
+            int[] leftArray = new int[middle - left + 1];
+            int[] rightArray = new int[right - middle];
+
+            Array.Copy(input, left, leftArray, 0, middle - left + 1);
+            Array.Copy(input, middle + 1, rightArray, 0, right - middle);
+
+            int i = 0;
+            int j = 0;
+            for (int k = left; k < right + 1; k++)
+            {
+                if (i == leftArray.Length)
+                {
+                    input[k] = rightArray[j];
+                    j++;
+                }
+                else if (j == rightArray.Length)
+                {
+                    input[k] = leftArray[i];
+                    i++;
+                }
+                else if (leftArray[i] <= rightArray[j])
+                {
+                    input[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    input[k] = rightArray[j];
+                    j++;
+                }
+            }
+        }
+    }
+}
